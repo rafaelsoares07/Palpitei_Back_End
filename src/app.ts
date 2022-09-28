294cbc4f0e4368eae7ed8d5f1bd9d2dd80226e12
rafaelsoares07 <rafaelsoares1936@gmail.com>
2022-09-28 13:13:55 -0300

    feat:Inicializando e exportando o App

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
+import express, { Router }  from "express";
+import cors from "cors";
+import "express-async-errors"
 
+import indexRouter from "./routes/indexRouter"
+
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(indexRouter)
+
+
+export default app 
