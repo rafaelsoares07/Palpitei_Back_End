1be7ae324ddf2ade3c55a2806d8133f8b2ec3aa8
rafaelsoares07 <rafaelsoares1936@gmail.com>
2022-09-30 00:33:52 -0300

    feat: Adicionando errorMiddleware no App

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "express-async-errors"
 
 import indexRouter from "./routes/indexRouter"
+import errorHandleMiddleware from "./middlewares/errorHandleMiddleware";
 
 
 const app = express()
@@ -10,6 +11,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(indexRouter)
+app.use(errorHandleMiddleware)
 
 
 export default app 
