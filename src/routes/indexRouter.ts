fc4f881c12871c7c251da36f9f28b44521d51497
rafaelsoares07 <rafaelsoares1936@gmail.com>
2022-09-30 16:20:02 -0300

    feat: Adicionando matchesRouter a indexRouter

--- src/routes/indexRouter.ts
+++ src/routes/indexRouter.ts
@@ -2,7 +2,8 @@ import  Router  from "express";
 const router = Router()
 
 import authRouter from "./authRouter"
-
+import matchesRouter from "./matchesRouter"
 router.use(authRouter)
+router.use(matchesRouter)
 
 export default router
