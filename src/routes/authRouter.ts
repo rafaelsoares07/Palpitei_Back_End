7f17c24543208b8d19b0346656ea4714673f63a4
rafaelsoares07 <rafaelsoares1936@gmail.com>
2022-09-30 00:40:38 -0300

    feat: Criando rotas de cadastro e login de usuários

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,8 +1,24 @@
 import Router from "express";
 const router = Router()
 
+//Controllers
 import * as authController from "../controllers/authController"
+//Shemas
+import {validateSchemaMiddleware}  from "../middlewares/validateSchemaMiddleware"
+import signinSchema from "../schemas/signinSchema";
+import signupSchema from "../schemas/signupSchema"
 
-router.get("/signup",authController.signup)
+router.post(
+"/signup",
+validateSchemaMiddleware(signupSchema),
+authController.signup
+)
+
+router.post(
+    "/signin",
+    validateSchemaMiddleware(signinSchema),
+    authController.signin
+)
+
 
 export default router
