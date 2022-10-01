c31b6b194c06d8b34deb04501b76f234b023bb4e
rafaelsoares07 <rafaelsoares1936@gmail.com>
2022-09-30 23:24:11 -0300

    feat: Alterando a forma como as partidas são retornadas

--- src/services/matchesService.ts
+++ src/services/matchesService.ts
@@ -3,13 +3,47 @@ import * as matchesRepository from "../repository/matchesRepository"
 export async function getMatchesByRound(){
 
     const result = await matchesRepository.getAll()
+
+    const objReturn = result.map((el)=>{
+        const aux = {
+            groupName:el.name,
+            roundOne:[
+                el.matches[0],
+                el.matches[1]
+            ],
+            roundTwo:[
+                el.matches[2],
+                el.matches[3]
+            ],
+            roundTree:[
+                el.matches[4],
+                el.matches[5]
+            ]
+        }
+        return aux
+    })
     
-    return result
+    return objReturn
 }
 
 export async function getMatchesByGroupId(groupId:number){
 
     const result = await matchesRepository.getMatchesByGroupId(groupId)
-    
-    return result
+
+    const objReturn = {
+        groupName:result[0].name,
+        roundOne:[
+            result[0].matches[0],
+            result[0].matches[1]
+        ],
+        roundTwo:[
+            result[0].matches[2],
+            result[0].matches[3]
+        ],
+        roundTree:[
+            result[0].matches[4],
+            result[0].matches[5]
+        ]
+    }
+    return objReturn
 }
