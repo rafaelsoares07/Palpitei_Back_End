82fff94425cb7919fa1c05d6c176299d3ce4da0f
rafaelsoares07 <rafaelsoares1936@gmail.com>
2022-09-30 23:16:10 -0300

    feat : Adicionando betsRouter no indexRouter

--- src/routes/indexRouter.ts
+++ src/routes/indexRouter.ts
@@ -3,7 +3,9 @@ const router = Router()
 
 import authRouter from "./authRouter"
 import matchesRouter from "./matchesRouter"
+import betsRouter from "./betsRouter"
 router.use(authRouter)
 router.use(matchesRouter)
+router.use(betsRouter)
 
 export default router
