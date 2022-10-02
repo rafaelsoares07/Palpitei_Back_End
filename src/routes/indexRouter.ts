9541d4c7a8c8a8a8fc467ab78b9981bdfca5d010
rafaelsoares07 <rafaelsoares1936@gmail.com>
2022-10-02 00:06:01 -0300

    feat: Adicionando adminRouter na indexRouter

--- src/routes/indexRouter.ts
+++ src/routes/indexRouter.ts
@@ -4,8 +4,11 @@ const router = Router()
 import authRouter from "./authRouter"
 import matchesRouter from "./matchesRouter"
 import betsRouter from "./betsRouter"
+import adminRouter from "./adminRouter"
+
 router.use(authRouter)
 router.use(matchesRouter)
 router.use(betsRouter)
+router.use(adminRouter)
 
 export default router
