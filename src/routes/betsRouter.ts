6bdac3bbec004e42a41effc464ca13e9a027e09e
rafaelsoares07 <rafaelsoares1936@gmail.com>
2022-10-02 15:22:34 -0300

    feat: Adição de schema joi em rota de criar um palpite

--- src/routes/betsRouter.ts
+++ src/routes/betsRouter.ts
@@ -4,9 +4,13 @@ const router = Router()
 import tokenValidation from "../middlewares/validateToken";
 
 import * as betsController from "../controllers/betsController"
+import { validateSchemaMiddleware } from "../middlewares/validateSchemaMiddleware";
+
+import createBetSchema from "../schemas/createBetSchema"
 
 router.post(
 "/bets",
+validateSchemaMiddleware(createBetSchema),
 tokenValidation,
 betsController.createBet
 )
