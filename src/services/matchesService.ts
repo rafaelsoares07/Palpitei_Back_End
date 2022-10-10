01d46a79d11c5f96189ec0d165810d9a08cd2668
rafaelsoares07 <rafaelsoares1936@gmail.com>
2022-10-10 00:19:35 -0300

    fix: Correção de bug no retorno das partidas

--- src/services/matchesService.ts
+++ src/services/matchesService.ts
@@ -5,23 +5,20 @@ export async function getMatchesByRound(){
     const result = await matchesRepository.getAll()
 
     const objReturn = result.map((el)=>{
+
+        const round1Filter1 = el.matches.filter((item)=>item.roundId===1)
+        const round1Filter2 = el.matches.filter((item)=>item.roundId===2)
+        const round1Filter3 = el.matches.filter((item)=>item.roundId===3)
+
         const aux = {
             groupName:el.name,
-            roundOne:[
-                el.matches[0],
-                el.matches[1]
-            ],
-            roundTwo:[
-                el.matches[2],
-                el.matches[3]
-            ],
-            roundTree:[
-                el.matches[4],
-                el.matches[5]
-            ]
+            roundOne:round1Filter1,
+            roundTwo:round1Filter2,
+            roundTree:round1Filter3
         }
         return aux
     })
+
     
     return objReturn
 }
@@ -30,20 +27,25 @@ export async function getMatchesByGroupId(groupId:number){
 
     const result = await matchesRepository.getMatchesByGroupId(groupId)
 
+    
+    const round1Filter1 = result[0].matches.filter((item)=>item.roundId===1)
+    const round1Filter2 = result[0].matches.filter((item)=>item.roundId===2)
+    const round1Filter3 = result[0].matches.filter((item)=>item.roundId===3)
+    
+
     const objReturn = {
         groupName:result[0].name,
-        roundOne:[
-            result[0].matches[0],
-            result[0].matches[1]
-        ],
-        roundTwo:[
-            result[0].matches[2],
-            result[0].matches[3]
-        ],
-        roundTree:[
-            result[0].matches[4],
-            result[0].matches[5]
-        ]
+        roundOne:round1Filter1,
+        roundTwo:round1Filter2,
+        roundTree:round1Filter3
     }
     return objReturn
+}
+
+
+export async function getMatchesByDay(day:string){
+
+    const result = await matchesRepository.getMatchesByDay(day)
+
+    return result
 }
